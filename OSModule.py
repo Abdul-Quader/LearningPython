0b6cd3667b92d10a2f19e51f3604adf6ee649c1a
AbdulQuaderQureshi <abdul.quader@wisdomtech.us>
2023-10-10 22:27:54 +0530

    Python OS Module

---
+++ OSModule.py
@@ -0,0 +1,29 @@
+#Program to show various Python OS Module 
+import os
+
+#Print Current Working Directory
+cwd = os.getcwd()
+print("Current working directory : ", cwd)
+
+#Display list of files and directories
+print(os.listdir())
+
+#Check the OS module
+print("The Operating system module is '% s'" % os.name)
+
+#Remove a file Test.py
+# os.remove(os.path.join(cwd,"Test.py"))
+
+#Opening a file and read
+try:
+
+    path = os.path.join(cwd,"README.md")
+    file = open(path, 'r')
+    print(file.read())
+    file.write("Learning Python")
+    # print(file.read())
+    file.close()
+
+except IOError:
+    print("File % s not found"% "README.md")
+
